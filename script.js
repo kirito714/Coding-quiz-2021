093890aad067d99db04ede11e4d557b7a89cf551
Joseph Amador <kirito.1986.714@gmail.com>
2021-06-03 10:05:47 -0700

    got answers and questions to be displayed on each question

--- script.js
+++ script.js
@@ -2,10 +2,117 @@ const startBtn = document.querySelector("#startBtn");
 const home = document.querySelector("#home");
 const quiz = document.querySelector("#quiz");
 const startquizbtn = document.querySelector("#startquizbtn");
-const questions = document.querySelector("#questions");
+const question = document.querySelector("#question");
 const qtime = document.querySelector("#qtime");
 const time = document.querySelector(".time");
+const scoreText = document.querySelector("#progressBarFull");
+const progressText = document.querySelector("#progressText");
+const answerText = Array.from(document.querySelectorAll(".answer-text"));
+var secondsLeft = 80;
+const highScoreScreen = document.querySelector("#highScoreScreen");
+const highScoreBtn = document.querySelector("#HighScores-btn");
 
+
+// CAPs note in JS shows that its going to be fixed.
+const SCORE_POINTS = 100;
+const MAX_QUEStIONS = 4;
+let intials = document.querySelector("#intials");
+let highScore = document.querySelector("#score");
+let score = 0;
+let acceptingAnswers = true;
+let availableQuestions = [];
+let currentQuestion = {};
+let questionCounter = 0;
+
+const questions = [
+  {
+    question: "Thor uses what weapon?",
+    choices: ["Sword", "Hammer", "Sniper Rifle", "Warship"],
+    answer: 1, // index of correct answer
+  },
+  {
+    question: "Iron Mans name is?",
+    choices: ["John Stark", "Tony Slark", "Morgan Freeman", "Tony Stark"],
+    answer: 3, // index of correct answer
+  },
+  {
+    question: "DeadPool's favorite food is ?",
+    choices: ["Pizza", "chimmy (Burrito)", "Burgers", "Salad"],
+    answer: 1, // index of correct answer
+  },
+  {
+    question: " Whats BatMans name?",
+    choices: ["John Wayne", "Bruce Banner", "Jim Carry", "Bruce Wayne"],
+    answer: 3, // index of correct answer
+  },
+];
+
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  // using a spread operator to include all available questions.
+  availableQuestions = [...questions];
+  getNewQuestion();
+  setTime();
+};
+// get new question function
+getNewQuestion = () => {
+  if (availableQuestions.length === 0 || questionCounter > MAX_QUEStIONS) {
+    //saves recent score to local storage
+    localStorage.setItem("mostRecentScore", score);
+    return;
+  }
+  // makes question counter go up by 1
+  questionCounter++;
+  // makes the counter go up plus one 2 of 4, 3 of 4 ect.
+  progressText.innerText = `question ${questionCounter} of ${MAX_QUEStIONS}`;
+  progressBarFull.style.width = `${(questionCounter / MAX_QUEStIONS) * 100}%`;
+  //  calculate the value of the question index
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  // keep track of the question user is on.
+  currentQuestion = availableQuestions[questionIndex];
+  // shows the question to display.
+  question.innerText = currentQuestion.question;
+
+  answerText.forEach((choice, i) => {
+    choice.innerText = currentQuestion.choices[i];
+  });
+  availableQuestions.splice(questionIndex, 1);
+
+  acceptingAnswers = true;
+};
+
+answerText.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    console.log(selectedChoice);
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    let classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+    console.log(classToApply, currentQuestion);
+
+    if (classToApply === "correct") {
+      incrementScore(SCORE_POINTS);
+    } else {
+      secondsLeft -= 20;
+    }
+    selectedChoice.parentElement.classList.add(classToApply);
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
 startBtn.addEventListener("click", function (e) {
   home.classList.add("hidden");
   quiz.classList.remove("hidden");
@@ -14,19 +121,31 @@ startBtn.addEventListener("click", function (e) {
 startquizbtn.addEventListener("click", function (e) {
   quiz.classList.add("hidden");
   qtime.classList.remove("hidden");
-  setTime();
+
+  startGame();
+});
+highScoreBtn.addEventListener("click", function (e) {
+  displayHighScoreScreen();
+  localStorage.getItem("mostRecentScore", score);
+  highScore.innerText = [];
 });
 
-var secondsLeft = 80;
+function displayHighScoreScreen() {
+  quiz.classList.add("hidden");
+  home.classList.add("hidden");
+  qtime.classList.add("hidden");
+  highScoreScreen.classList.remove("hidden");
+}
+
 function setTime() {
   var timerInterval = setInterval(function () {
     secondsLeft--;
     time.textContent = secondsLeft + " Time Left!";
 
-    if (secondsLeft === 0) {
+    if (secondsLeft <= 0) {
+      secondsLeft = 0;
       clearInterval(timerInterval);
-
-    
+      displayHighScoreScreen();
     }
   }, 1000);
 }
